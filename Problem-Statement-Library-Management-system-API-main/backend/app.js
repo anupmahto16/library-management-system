4d20235eb4b04b16cd0b71c5e24a5cf1e385be55
anupjan16 <anupmahto.in@gmail.com>
2024-10-06 19:35:16 +0530

    added file

--- Problem-Statement-Library-Management-system-API-main/backend/app.js
+++ Problem-Statement-Library-Management-system-API-main/backend/app.js
@@ -8,13 +8,16 @@ const errorMiddleware = require('./middleware/errorMiddleware');
 require('dotenv').config();
 
 const app = express();
+
 connectDB();
 
 app.use(express.json());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/authors', authorRoutes);
 app.use('/api/books', bookRoutes);
 app.use('/api/borrowings', borrowingRoutes);
+
 app.use(errorMiddleware);
 
 module.exports = app;
